f02b9cda97d4199c6afab1df11edf0d75538e844
JAMES223333 <kyawzinhien2104@gmail.com>
2025-05-22 15:55:58 +0630

    poolito

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    hero_area = HeroArea.objects.all().order_by('-id')[:1]
+    contex = {
+        'hero_area': hero_area
+    }
+    return render(request, 'index.html',contex)
